5be961926f3b83d2a620b1f5076adb2e1f897c8b
Jasurbek Kalandarov <jkalandarov@gmail.com>
2022-09-11 18:02:55 +0500

    Working server setup

--- server/app.js
+++ server/app.js
@@ -1,27 +1,29 @@
 const express = require('express');
+const logger = require('morgan');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const routes = require('./routes/index')
+const routes = require('./routes/index');
 
 const app = express();
+app.use(logger('api'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // router
-app.use('/dev', routes);
+app.use('/api', routes);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 404).json({
-        message: 'No such route exists',
+        message: '404 - No such route exists',
     });
 });
 
 // error handler
 app.use(function (err, req, res, next) {
     res.status(err.status || 500).json({
-        message: 'Error Message',
+        message: '500 - Internal Server Error'
     });
 });
 
