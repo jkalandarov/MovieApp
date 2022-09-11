15445b86bd50f31098cb0922916ef9ec0aa4e429
Jasurbek Kalandarov <jkalandarov@gmail.com>
2022-09-11 17:30:15 +0500

    Added server and app files

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+require('dotenv').config()
+
+const routes = require('./routes/index')
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// router
+app.use('/dev', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    res.status(err.status || 404).json({
+        message: 'No such route exists',
+    });
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500).json({
+        message: 'Error Message',
+    });
+});
+
+module.exports = app;
