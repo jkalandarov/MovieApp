4860487a8d44b7b65f05dec1b6db17da9631c6cd
Jasurbek Kalandarov <jasurbek_kalandarov@epam.com>
2022-12-26 16:06:31 +0500

    create test route

--- controllers/home.page.js
+++ controllers/home.page.js
@@ -3,3 +3,7 @@ const db = require('../db/db');
 exports.home = async (req, res) => {
   res.status(200).json(db.mainPage);
 }
+
+exports.test = async (req, res) => {
+  res.status(200).send('Working! 👌');
+}
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const Home = require('../controllers/home.page');
 
 router.get('/', Home.home);
+router.get('/test', Home.test);
 
 module.exports = router;
